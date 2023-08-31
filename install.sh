83e0f814727c6e6ed16f85cd7fc7c3f17d54ff14
jomel.madia <jomel.madia@gmail.com>
2023-08-31 18:53:42 +0800

    Add directory for download

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-wget "https://raw.githubusercontent.com/jomelmadia/neovim-config/main/init.vim"
+wget -P ~/.config/nvim/ "https://raw.githubusercontent.com/jomelmadia/neovim-config/main/init.vim"
