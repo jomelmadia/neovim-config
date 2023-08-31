2b6fc8aefc1fa6f1907b0da47f4b1740d3a296c4
jomel.madia <jomel.madia@gmail.com>
2023-08-31 18:50:34 +0800

    Added install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget "https://raw.githubusercontent.com/jomelmadia/neovim-config/main/init.vim"
